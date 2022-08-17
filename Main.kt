8f206b06365135699c38713d423990a6a2381b9b
Roberto Pasquali <pasquali.roberto@gmail.com>
2022-08-17 22:07:38 +0200

    5/6 - Keep track of the supplies

--- Main.kt
+++ Main.kt
@@ -10,16 +10,19 @@ class CoffeeMachine(
     companion object {
         const val WATER_PER_ESPRESSO = 250
         const val COFFEE_BEANS_PER_ESPRESSO = 16
+        const val CUPS_PER_ESPRESSO = 1
         const val ESPRESSO_COST = 4
 
         const val WATER_PER_LATTE = 350
         const val MILK_PER_LATTE = 75
         const val COFFEE_BEANS_PER_LATTE = 20
+        const val CUPS_PER_LATTE = 1
         const val LATTE_COST = 7
 
         const val WATER_PER_CAPPUCCINO = 200
         const val MILK_PER_CAPPUCCINO = 100
         const val COFFEE_BEANS_PER_CAPPUCCINO = 12
+        const val CUPS_PER_CAPPUCCINO = 1
         const val CAPPUCCINO_COST = 6
     }
 
@@ -34,15 +37,18 @@ class CoffeeMachine(
     fun turnOn() {
         fun readMainMenuChoice(): String = readln()
 
-        printStats()
-        printMenu()
-        when (readMainMenuChoice()) {
-            "buy" -> buyAction()
-            "fill" -> fillAction()
-            "take" -> takeAction()
-            else -> {
-                println("invalid choice")
-                return
+        while (true) {
+            printMenu()
+            when (readMainMenuChoice()) {
+                "buy" -> buyAction()
+                "fill" -> fillAction()
+                "take" -> takeAction()
+                "remaining" -> printStats()
+                "exit" -> return
+                else -> {
+                    println("invalid choice")
+                    return
+                }
             }
         }
     }
@@ -59,52 +65,121 @@ class CoffeeMachine(
 
     private fun printMenu() {
         println()
-        print("Write action (buy, fill, take): ")
+        print("Write action (buy, fill, take, remaining, exit): ")
     }
 
     private fun buyAction() {
         fun printBuyMenu() {
-            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
         }
 
-        fun isValidChoice(choice: Int): Boolean = choice in 1..3
+        fun isValidChoice(choice: String): Boolean = choice.first() in '1'..'3' || choice == "back"
 
-        fun readBuyMenuChoice(): Int =
+        fun readBuyMenuChoice(): String =
             try {
                 printBuyMenu()
-                val coffeeChoice = readln().toInt()
+                val coffeeChoice = readln().trim()
                 if (!isValidChoice(coffeeChoice)) throw Exception("coffee choice not valid")
                 coffeeChoice
-            } catch (e: NumberFormatException) {
-                println("Please insert a valid number")
-                readBuyMenuChoice()
             } catch (e: Exception) {
                 println("Please enter a valid choice")
                 readBuyMenuChoice()
             }
 
         when (readBuyMenuChoice()) {
-            1 -> makeCoffee(CoffeeType.ESPRESSO)
-            2 -> makeCoffee(CoffeeType.LATTE)
-            3 -> makeCoffee(CoffeeType.CAPPUCCINO)
+            "1" -> if (areResourcesEnoughFor(CoffeeType.ESPRESSO)) {
+                println("I have enough resources, making you a coffee!")
+                makeCoffee(CoffeeType.ESPRESSO)
+            } else printFailingCauseFor(
+                CoffeeType.ESPRESSO
+            )
+            "2" -> if (areResourcesEnoughFor(CoffeeType.LATTE)) {
+                println("I have enough resources, making you a coffee!")
+                makeCoffee(CoffeeType.LATTE)
+            } else printFailingCauseFor(
+                CoffeeType.LATTE
+            )
+            "3" -> if (areResourcesEnoughFor(CoffeeType.CAPPUCCINO)) {
+                println("I have enough resources, making you a coffee!")
+                makeCoffee(CoffeeType.CAPPUCCINO)
+            } else printFailingCauseFor(
+                CoffeeType.CAPPUCCINO
+            )
+            "back" -> return
         }
-        printStats()
+    }
+
+    private fun areResourcesEnoughFor(coffeeType: CoffeeType): Boolean {
+        val espressoConditions =
+            water >= WATER_PER_ESPRESSO && coffeeBeans >= COFFEE_BEANS_PER_ESPRESSO && cups >= CUPS_PER_ESPRESSO
+        val latteConditions =
+            water >= WATER_PER_LATTE && milk >= MILK_PER_LATTE && coffeeBeans >= COFFEE_BEANS_PER_LATTE && cups >= CUPS_PER_LATTE
+        val cappuccinoConditions =
+            water >= WATER_PER_CAPPUCCINO && milk >= MILK_PER_CAPPUCCINO && coffeeBeans >= COFFEE_BEANS_PER_CAPPUCCINO && cups >= CUPS_PER_CAPPUCCINO
+
+        return when (coffeeType) {
+            CoffeeType.ESPRESSO -> espressoConditions
+            CoffeeType.LATTE -> latteConditions
+            CoffeeType.CAPPUCCINO -> cappuccinoConditions
+        }
+    }
+
+    private fun printFailingCauseFor(coffeeType: CoffeeType) {
+        when (coffeeType) {
+            CoffeeType.ESPRESSO -> {
+                when {
+                    water < WATER_PER_ESPRESSO -> println("Sorry, not enough water!")
+                    coffeeBeans < COFFEE_BEANS_PER_ESPRESSO -> println("Sorry, not enough coffee beans!")
+                    cups < CUPS_PER_ESPRESSO -> println("Sorry, not enough disposable cups!")
+                }
+            }
+            CoffeeType.LATTE -> {
+                when {
+                    water < WATER_PER_LATTE -> println("Sorry, not enough water!")
+                    milk < MILK_PER_LATTE -> println("Sorry, not enough milk!")
+                    coffeeBeans < COFFEE_BEANS_PER_LATTE -> println("Sorry, not enough coffee beans!")
+                    cups < CUPS_PER_LATTE -> println("Sorry, not enough disposable cups!")
+                }
+            }
+            CoffeeType.CAPPUCCINO -> {
+                when {
+                    water < WATER_PER_CAPPUCCINO -> println("Sorry, not enough water!")
+                    milk < MILK_PER_CAPPUCCINO -> println("Sorry, not enough milk!")
+                    coffeeBeans < COFFEE_BEANS_PER_CAPPUCCINO -> println("Sorry, not enough coffee beans!")
+                    cups < CUPS_PER_CAPPUCCINO -> println("Sorry, not enough disposable cups!")
+                }
+            }
+        }
     }
 
     private fun makeCoffee(coffeeType: CoffeeType) {
         when (coffeeType) {
             CoffeeType.ESPRESSO -> {
-                updateMaterials(WATER_PER_ESPRESSO, 0, COFFEE_BEANS_PER_ESPRESSO, 1, ESPRESSO_COST, Operation.BUY)
+                updateMaterials(
+                    WATER_PER_ESPRESSO,
+                    0,
+                    COFFEE_BEANS_PER_ESPRESSO,
+                    CUPS_PER_ESPRESSO,
+                    ESPRESSO_COST,
+                    Operation.BUY
+                )
             }
             CoffeeType.LATTE -> {
-                updateMaterials(WATER_PER_LATTE, MILK_PER_LATTE, COFFEE_BEANS_PER_LATTE, 1, LATTE_COST, Operation.BUY)
+                updateMaterials(
+                    WATER_PER_LATTE,
+                    MILK_PER_LATTE,
+                    COFFEE_BEANS_PER_LATTE,
+                    CUPS_PER_LATTE,
+                    LATTE_COST,
+                    Operation.BUY
+                )
             }
             CoffeeType.CAPPUCCINO -> {
                 updateMaterials(
                     WATER_PER_CAPPUCCINO,
                     MILK_PER_CAPPUCCINO,
                     COFFEE_BEANS_PER_CAPPUCCINO,
-                    1,
+                    CUPS_PER_CAPPUCCINO,
                     CAPPUCCINO_COST,
                     Operation.BUY
                 )
@@ -112,7 +187,14 @@ class CoffeeMachine(
         }
     }
 
-    private fun updateMaterials(water: Int = 0, milk: Int = 0, coffeeBeans: Int = 0, cups: Int = 0, money: Int = 0, operation: Operation) {
+    private fun updateMaterials(
+        water: Int = 0,
+        milk: Int = 0,
+        coffeeBeans: Int = 0,
+        cups: Int = 0,
+        money: Int = 0,
+        operation: Operation
+    ) {
         when (operation) {
             Operation.BUY -> {
                 this.water -= water
@@ -164,13 +246,11 @@ class CoffeeMachine(
 
         val (refilledWater, refilledMilk, refilledCoffeeBeans, refilledCups) = readFillMenuChoice()
         updateMaterials(refilledWater, refilledMilk, refilledCoffeeBeans, refilledCups, operation = Operation.FILL)
-        printStats()
     }
 
     private fun takeAction() {
         println("I gave you \$$money")
         updateMaterials(operation = Operation.TAKE)
-        printStats()
     }
 }
 
